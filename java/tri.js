84473735406fcd2079b6439d07f0e061a05d71d2
TimofeyTTT <45222181+TimofeyTTT@users.noreply.github.com>
2018-12-06 11:07:59 +0300

    Update tri.js

--- java/tri.js
+++ java/tri.js
@@ -1,5 +1,21 @@
-var isTriangle = function(sideOne, sideTwo, sideThree) {	var isTriangle = function(sideOne, sideTwo, sideThree) {
-  return sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)	  return sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)
-}	};
- $(document).ready(function() {	$(document).ready(function() {
-  $("form#triangle").submit(function(event) {	  $("form#triangle").submit(function(event) {
+  function triangleType(sides){
+    var a = parseInt(document.getElementById('type').value);
+    var b= parseInt(document.getElementById('typeB').value);
+    var c= parseInt(document.getElementById('typeC').value);
+var sides=[];
+sides.push(a, b, c);
+var t = sides[0];
+var s = sides[1];
+var r = sides[2];
+      if(t+s<=r || s+r<=t || t+r<=s){
+        alert("not a Triangle");
+      }else if(t===s && s===r && r===t){
+        alert("Equilateral Triangle");
+    } else if(t==s || r==s || r==t){
+      alert("Isosceles Triangle");
+    } else if(t!==s && s!==r && t!==r){
+        alert("Scalene Triangle");
+      }else{
+        alert("error");
+      }
+}
